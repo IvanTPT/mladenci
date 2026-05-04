4892c55388bc7e8659d0e525457ae3b211229648
IvanTPT <ikrstic@transperfect.com>
2026-05-04 08:39:36 +0200

    Update Astro config for Tailwind CSS integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
-import tailwind from '@astrojs/tailwind';
-import ViteYaml from '@modyfi/vite-plugin-yaml';
+import tailwindcss from "@tailwindcss/vite";
 
+// https://astro.build/config
 export default defineConfig({
-  output: 'server', // Потребно за Cloudflare SSR
-  adapter: cloudflare(),
-  integrations: [tailwind()],
   vite: {
-    plugins: [ViteYaml()],
+    plugins: [tailwindcss()],
   },
 });
