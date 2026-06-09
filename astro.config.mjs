b550b54948d9c50702e6167d35b1517f137edea7
IvanTPT <ikrstic@transperfect.com>
2026-06-09 14:03:26 +0200

    umesto astro.url astro.site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 import ViteYaml from '@modyfi/vite-plugin-yaml'; 
 // https://astro.build/config
 export default defineConfig({
+  // ДОДАЈ ОВУ ЛИНИЈУ И УНЕСИ СВОЈ ПРАВИ ЛИНК:
+  site: 'https://mladenci.emedia-rs.workers.dev/',
   vite: {
     plugins: [tailwindcss()],
     plugins: [ViteYaml()],
